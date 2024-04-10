d56fc2e2ea508b1b30f9b18c0165e8c47881473d
Erertertet <erertertet@gmail.com>
2024-04-11 00:34:51 +0100

    covers Configs.java

--- src/main/resources/assets/tweakeroo/lang/working_directory/auto_transform.py
+++ src/main/resources/assets/tweakeroo/lang/working_directory/auto_transform.py
@@ -1,11 +1,16 @@
 import re
 
 lines = open('src\main\\resources\\assets\\tweakeroo\lang\working_directory\Configs.java').read()
-
 pattern = r'public static final [^(]*\(([^)\n]*)\)'
-
 matches = re.findall(pattern, lines)
 
+names = []
+
 for match in matches:
-    literal = match.split(",")
-    print(literal[0], literal[-1])
+    literal = match.split(", ")
+    # print(literal[0], literal[-1])
+    names.append((literal[0], literal[-1]))
+
+for name in names:
+    print(f"{name[0]}: \"测试\",")
+
