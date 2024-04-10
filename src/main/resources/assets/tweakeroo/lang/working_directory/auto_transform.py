e65ff8e7b056b9add40be7c688690d281b8f0d83
Erertertet <erertertet@gmail.com>
2024-04-11 00:55:55 +0100

    added all entries name except single player

--- src/main/resources/assets/tweakeroo/lang/working_directory/auto_transform.py
+++ src/main/resources/assets/tweakeroo/lang/working_directory/auto_transform.py
@@ -1,6 +1,6 @@
 import re
 
-lines = open('src\main\\resources\\assets\\tweakeroo\lang\working_directory\Configs.java').read()
+lines = open('src\main\java\\fi\dy\masa\\tweakeroo\config\Hotkeys.java', 'r').read()
 pattern = r'public static final [^(]*\(([^)\n]*)\)'
 matches = re.findall(pattern, lines)
 
@@ -12,5 +12,36 @@ for match in matches:
     names.append((literal[0], literal[-1]))
 
 for name in names:
+    ...
     print(f"{name[0]}: \"测试\",")
 
+quit()
+
+lines = open('src\main\java\\fi\dy\masa\\tweakeroo\config\FeatureToggle.java', 'r').read()
+pattern = r'[^(]*\(([\"[a-zA-Z]*\",[^\)]*]*)'
+matches = re.findall(pattern, lines)
+
+names = []
+
+for match in matches:
+    literal = match.split(", ")
+    # print(literal[0], literal[-1])
+    names.append((literal[0], literal[-1]))
+
+for name in names:
+    ...
+    # print(f"{name[0]}: \"测试\",")
+
+lines = open('src\main\java\\fi\dy\masa\\tweakeroo\config\FeatureToggle.java', 'r').read()
+pattern = r'[^(]*\(([\"[a-zA-Z]*\",[^\)]*]*)'
+matches = re.findall(pattern, lines)
+
+names = []
+
+for match in matches:
+    literal = match.split(", ")
+    # print(literal[0], literal[-1])
+    names.append((literal[0], literal[-1]))
+
+for name in names:
+    print(f"{name[0]}: \"测试\",")
